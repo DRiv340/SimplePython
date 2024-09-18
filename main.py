775b5506b14bf38fbf91239ef8c80d58c7e641d6
DRiv340 <danielr94@live.com>
2024-09-18 19:37:01 -0400

    changed the python program for one that does system calls

--- main.py
+++ main.py
@@ -1,40 +1,121 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys,os
 
-import threading,time
+def exe(command):
+   try:
+       if "cd " in command: # If the user wants to change directories
+            x = command.split(" ")
+            dir = x[1]
+            change_dirdown(os.getcwd(),dir)
 
-full = threading.Lock()
-mutex = threading.Lock()
+       elif "cd.."in command:
+           change_dirup(os.getcwd())
 
-def producer(): # This is thread 2
- for x in range(1000):
-  full.acquire() #acquires full after t1 releases
-  mutex.acquire()
+       elif "|" in command: # If the user wants to create a pipe for two commands/// Move this else if to if > statement
+           cms = command.split("|") #split into first command and second command
+           c1 = cms[0].split(" ") #Split the first command by spaces to get the command and the flags
+           c2 = cms[1].split(" ") #Split the second command by spaces to get the command and the flags
 
-  print("put")
-  time.sleep(1)
+           r = os.fork() # Create the new process
 
-  full.release()
-  mutex.release()
+           if r < 0: #Error
+             print("Error")
 
-def consumer(): # This is thread 1
- for x in range(1000):
-  mutex.acquire() #acquires mutex first and not full
-  full.acquire()
+           elif r == 0: #Child will only read
+             os.dup2(pw,1,True)
+             os.close(pw)
+             os.execve("D:/Cygwin/bin/"+c1[0],[c1[0]],os.environ)
+             os.close(1)
 
-  print("get")
-  time.sleep(1)
+           else:# Parent
+             os.dup2(pr, 0, True)
+             os.close(pr)  # Parent process will only write
+             os.execve("D:/Cygwin/bin/"+c2[1],[c2[1]],os.environ)
+             os.close(0)
+             return
+       else:
+           if ">" in command:
+             redir = command.split(">")
+             redir[1] = redir[1].replace(" ","")
+             redir[0] = redir[0].replace(" ", "")
+             sys.stdout = open(redir[1],'w')
 
-  mutex.release() # changing the order in which I release here will allow each thread to aqcuire only one lock
-  full.release() # causing deadlock since t2 may acquire full and t1 may acquire mutex
+           elif "<" in command:
+             redir = command.split("<")
+             redir[1] = redir[1].replace(" ","")
+             sys.stdin = open(redir[1],'R')
+
+           x = command.split(" ")
+           cmnd = x[0]
+           r = os.fork()
+           if r < 0:
+               print("There was an error with the fork")
+           elif r == 0:
+               os.execve("D:/Cygwin/bin/"+cmnd,x,os.environ)
+           else:
+               if "&" not in command:
+                   os.wait()
+
+               return
+   except FileNotFoundError:
+       print("The command was not found")
+
+def change_dirdown(current,dir):
+    try:
+        os.chdir(current+"/"+dir)
+    except NotADirectoryError:
+        print("Could not find directory")
+
+def change_dirup(current): #Change directory when the input is 0
+    try:
+        i = (len(current)-1)
+        c=current[i]
+        while c != "/":
+            i -= 1
+            c=current[i]
+
+        newd = current[0:i]
+
+        if newd != "D:":
+            os.chdir(newd)
+            return
+        else:
+            print("Can't go up in the directories")
+            return
+    except NotADirectoryError:
+        print("Could not find directory")
 
 
-t1 = threading.Thread(target= consumer,)
-t2 = threading.Thread(target= producer,)
-t1.start()
-t2.start()
 
+dir = "D:/Cygwin/bin"
+os.chdir(dir)
+
+pr,pw = os.pipe()
+sys.ps1="$$$"
+
+while True:
+
+    print(sys.ps1.encode())
+    c = input()
+
+    if c == "file":
+        print("file path:")
+        pth = input()
+        f = open(pth,"r")
+        for line in f:
+            if "#" in line[0]:
+                None
+            else:
+                exe(line)
+            print(line)
+
+#    elif c == "path":
+
+    elif c == "quit":
+        break
+
+    else:
+        exe(c)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
